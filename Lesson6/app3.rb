64d792a4388f002cc6cb36106f7b171ef5324e22
Морозов Иван Александрович <ivan.morozov@tele2.ru>
2020-03-06 10:55:41 +0600

    Lesson_6_tasks

--- Lesson6/app3.rb
+++ Lesson6/app3.rb
@@ -7,7 +7,7 @@ input = gets.to_i
 computer = rand(0..2)
 
 puts "Вы выбрали: #{game[input]}"
-puts "Вы выбрали: #{game[computer]}"
+puts "Компьютер выбрал: #{game[computer]}"
 
 if game[input] == "камень" && game[computer] == "ножницы"
   puts "Поздравляем, ты победил"
@@ -19,5 +19,12 @@ elsif game[input] == "бумага" && game[computer] == "бумага"
   puts "Ничья"
 elsif game[input] == "ножницы" && game[computer] == "камень"
   puts "Компьютер победил"
-if game[input] == "ножницы" && game[computer] == "бумага"
+elsif game[input] == "ножницы" && game[computer] == "бумага"
   puts "Поздравляем, ты победил"
+elsif game[input] == "камень" && game[computer] == "бумага"
+  puts "Компьютер победил"
+elsif game[input] == "бумага" && game[computer] == "ножницы"
+  puts "Компьютер победил"
+elsif game[input] == "бумага" && game[computer] == "камень"
+  puts "Поздравляем, ты победил"
+end
